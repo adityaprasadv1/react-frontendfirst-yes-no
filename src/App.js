47bb9cec0b6334c8dfe39d7488db70ff82a71d96
Aditya V <adityaprasadv@gmail.com>
2021-06-05 12:13:49 -0400

    Add Profile component to App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from "./logo.svg";
 import LoginButton from "./components/LoginButton";
+import { useAuth0 } from "@auth0/auth0-react";
 import "./App.css";
+import Profile from "./components/Profile";
 
 function App() {
+  const { isAuthenticated } = useAuth0();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <LoginButton />
+        {isAuthenticated ? <Profile /> : <LoginButton />}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
